145a067c720fd27b86f0d9ea6c3c0adb8fee8f7c
Andrii Shymko <55424136+BilliMorgan@users.noreply.github.com>
2023-05-19 16:12:57 -0400

    Connected openIA model

--- pages/post/new.js
+++ pages/post/new.js
@@ -1,21 +1,60 @@
+import { useState } from "react"
 import { withPageAuthRequired } from "@auth0/nextjs-auth0"
 import { AppLayout } from "../../components/AppLayout"
 
 export default function NewPost(props) {
   console.log("TESTING PROPS", props)
-  const handleClick = async () => {
+
+  const [topic, setTopic] = useState("")
+  const [keywords, setKeyWords] = useState("")
+
+  const [postContent, setPostContent] = useState("")
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
     const response = await fetch(`/api/generatePost`, {
       method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ topic, keywords }),
     })
     const json = await response.json()
-    console.log("RESPONSE: ", json)
+    console.log("RESULT: ", json.post.postContent)
+    setPostContent(json.post.postContent)
   }
   return (
     <div>
-      <h1>this is new post page</h1>
-      <button className="btn" onClick={handleClick}>
-        Generate
-      </button>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            <strong>Generate a blog post on the topic of:</strong>
+          </label>
+          <textarea
+            className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
+            value={topic}
+            onChange={(e) => setTopic(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>
+            <strong>Targeting the following keywords:</strong>
+          </label>
+          <textarea
+            className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
+            value={keywords}
+            onChange={(e) => setKeyWords(e.target.value)}
+          />
+        </div>
+
+        <button className="btn" type="submit">
+          Generate
+        </button>
+      </form>
+      <div
+        className="max-w-screen-sm p-10"
+        dangerouslySetInnerHTML={{ __html: postContent }}
+      ></div>
     </div>
   )
 }
