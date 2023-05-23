029140f47ac5847251aefd57851ce044c2ab2559
Andrii Shymko <55424136+BilliMorgan@users.noreply.github.com>
2023-05-23 15:28:47 -0400

    WIP started outsorcing getAppProps functionality

---
+++ utils/getAppProps.js
@@ -0,0 +1,11 @@
+import { getSession } from "@auth0/nextjs-auth0"
+import clientPromise from "../lib/mongodb"
+
+export const getAppProps = async (ctx) => {
+  const userSession = await getSession(ctx.req, ctx.res)
+  const client = await clientPromise
+  const db = client.db("BlogGenerator")
+  const user = await db.collection("users").findOne({
+    auth0Id: userSession.user.sub,
+  })
+}
