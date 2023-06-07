19dffc6b5a7427a5aaebc6e8db545b6203347be4
Andrii Shymko <55424136+BilliMorgan@users.noreply.github.com>
2023-06-07 17:26:42 -0400

    delete post functionality is done

--- pages/post/[postId].js
+++ pages/post/[postId].js
@@ -6,9 +6,31 @@ import { ObjectId } from "mongodb"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faHashtag } from "@fortawesome/free-solid-svg-icons"
 import { getAppProps } from "../../utils/getAppProps"
+import { useState } from "react"
+import { useRouter } from "next/router"
 
 export default function Post(props) {
   console.log("PROPS: ", props)
+  const router = useRouter()
+  const [showDeleteConfirm, setShowDeleteConfirm] = useState(false)
+
+  const handleDeleteConfirm = async () => {
+    try {
+      const response = await fetch(`/api/deletePost`, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ postId: props.id }),
+      })
+      const json = await response.json()
+      if (json.success) {
+        router.replace(`/post/new`)
+      }
+    } catch (error) {
+      console.log("DELETE POST ERROR", error)
+    }
+  }
   return (
     <div className="overflow-auto h-full">
       <div className="max-w-screen-sm mx-auto">
@@ -34,6 +56,38 @@ export default function Post(props) {
           Blog post
         </div>
         <div dangerouslySetInnerHTML={{ __html: props.postContent || "" }} />
+        <div className="mt-4">
+          {!showDeleteConfirm && (
+            <button
+              className="btn bg-red-600 hover:bg-red-700"
+              onClick={() => setShowDeleteConfirm(true)}
+            >
+              Delete post
+            </button>
+          )}
+          {!!showDeleteConfirm && (
+            <div>
+              <p className="p-2 bg-red-300 text-center">
+                Are you sure you want to delete this post? This action is
+                irreversible
+              </p>
+              <div className="grid grid-cols-2 gap-2">
+                <button
+                  className="btn bg-stone-600 hover:bg-stone-700"
+                  onClick={() => setShowDeleteConfirm(false)}
+                >
+                  cancel
+                </button>
+                <button
+                  className="btn bg-red-600 hover:bg-red-700"
+                  onClick={handleDeleteConfirm}
+                >
+                  confirm delete
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   )
